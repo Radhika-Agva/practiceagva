b46b76f10562139730b32c64c5e734d9c1c229ee
radhika130801 <radhikaiyengar@ddtech.in>
2022-11-04 17:14:18 +0530

    Video Display according to viewport

--- Documents/Radhika/aspectratio/task5/src/Video.js
+++ Documents/Radhika/aspectratio/task5/src/Video.js
@@ -1,15 +1,18 @@
-import React,{useRef,useEffect} from 'react'
-//import {AspectRatio} from 'react-aspect-ratio'
-//import ReactPlayer from 'react-player'
+import React,{useRef,useEffect, useState} from 'react'
 import video from './videos/Videofile.mp4'
 import Sample from './videos/Sample.mp4'
+import New from './videos/New.mp4'
 
 function Video(){
-    let height = window.innerHeight;
-    let width = window.innerWidth;
-    
-    const videoRef = useRef(null);
-    const videoRef2 = useRef(null);
+
+  const videoRef = useRef(null);
+  const videoRef2 = useRef(null);
+  const videoRef3 = useRef(null);
+
+  const[windowSize,setWindowSize] = useState({
+    height:undefined,
+    width:undefined,
+  });
 
     useEffect(() => {
         let options = {
@@ -34,20 +37,45 @@ function Video(){
               }
             });
           };
+          let handlePlay3 = (entries, observer) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                videoRef3.current.play();
+              } else {
+                videoRef3.current.pause();
+              }
+            });
+          };
       
           let observer = new IntersectionObserver(handlePlay, options);
           let observer2 = new IntersectionObserver(handlePlay2, options);
+          let observer3 = new IntersectionObserver(handlePlay3, options);
+
 
       
           observer.observe(videoRef.current);
           observer2.observe(videoRef2.current);
+          observer3.observe(videoRef3.current);
 
-        });
-    console.log(height,width)
+
+          function handleResize(){
+            setWindowSize({
+              height:window.innerHeight,
+              width: window.innerWidth
+            });
+          }
+          window.addEventListener('resize', handleResize);
+
+          handleResize();
+
+          return() =>window.removeEventListener("resize",handleResize);
+        },[]);
     return(
     <>
-    <video ref={videoRef} src={video} height={window.innerHeight} width={window.innerWidth} loop></video>
-    <video ref={videoRef2} src={Sample} height={window.innerHeight} width={window.innerWidth} loop></video>
+    <video ref={videoRef} src={video} height={windowSize.height} width={windowSize.width} loop></video>
+    <video ref={videoRef2} src={Sample} height={windowSize.height} width={windowSize.width} loop></video>
+    <video ref={videoRef3} src={New} height={windowSize.height} width={windowSize.width} loop></video>
+
     </>
     );
 }
